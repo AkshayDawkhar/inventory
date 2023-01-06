fc0a87cde556ff66a14e4e10fb8cc53a4d5f03c0
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-06 23:35:08 +0530

    working on serializer

--- inventory/productapi/serializers.py
+++ inventory/productapi/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 class low(serializers.CharField):
     def to_internal_value(self, data):
         return data.lower()
-
+a=[1,2,3,4,5,6]
 class Plistserializer(serializers.Serializer):
     fname=serializers.IntegerField()
     mname=serializers.CharField()
@@ -11,6 +11,6 @@ class Plistserializer(serializers.Serializer):
     rname=serializers.ListField(child=serializers.ListField(child=serializers.IntegerField()))
     # rname=ListField(child=serializers.IntegerField())
     def validate(self, data):
-        if data['fname'] is 0:
+        if data['fname'] not in a:
             raise serializers.ValidationError("fname is 0")
         return data
--- inventory/productapi/views.py
+++ inventory/productapi/views.py
@@ -8,10 +8,10 @@ from productapi.serializers import Plistserializer
 
 @api_view(['GET','POST'])
 def productlist(request):
-    d={'fname':10,'mname':'a','lname':'1','rname':[['1']]}
+    d={'fname':1,'mname':'a','lname':'1','rname':[['1']]}
     pls= Plistserializer(data=d)
     if pls.is_valid():
-        print('------------------------------------------valid',uuid.uuid4(),uuid.uuid1(),sep='\n')
+        print('------------------------------------------valid',sep='\n')
         return Response(pls.data,status=201)
     else:
         print('---------------------------------------not valid',pls.data.get('fname'),sep='\n')
