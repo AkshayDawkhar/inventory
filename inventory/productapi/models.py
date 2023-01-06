cfaa9758f95a2845d54115ea7a3cc39a6d3600d9
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-07 01:26:39 +0530

    created model for cassnadra

--- inventory/productapi/models.py
+++ inventory/productapi/models.py
@@ -1,3 +1,11 @@
-from django.db import models
-
+# from django.db import models
+from cassandra.cqlengine.models import Model,columns
 # Create your models here.
+class mccdr(Model):
+    __connection__= 'cluster'
+    __table_name__='mccdr'
+    __keyspace__='model1'
+    fname=columns.Integer(primary_key=True)
+    # mname=columns.Text()
+    lname=columns.Text()
+    oname=columns.Text()
