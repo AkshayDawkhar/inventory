0b5fbb9c4a453cd024c16301c2ab7ac6cc4a3cc5
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-09 00:00:53 +0530

    small change working with data retrieval

--- inventory/productapi/views.py
+++ inventory/productapi/views.py
@@ -6,13 +6,14 @@ from productapi.models import ProductList
 from productapi.serializers import Plistserializer
 from cassandra.cqlengine import columns, connection
 from cassandra.cluster import Cluster
-
+from cassandra.query import dict_factory
 # from rest_framework.serializers import Serializer
 # Create your views here.
 cluster = Cluster(['127.0.0.1'])
 sec = cluster.connect()
 con = connection.register_connection('cluster',session=sec)
-
+secs = cluster.connect('model1')
+sec.row_factory = dict_factory
 @api_view(['GET','POST'])
 def productlist(request):
     d={'pname': 'a', 'color': 'black', 'category': 'mic', 'pid':uuid.uuid4(), 'required_iteams': ['a', 'b']}
@@ -24,7 +25,15 @@ def productlist(request):
         print('------------------------------------------valid',sep='\n')
         p=ProductList(**pls.data)
         p.save()
-        return Response(pls.data,status=201)
+        #a=ProductList.all()
+        rs= sec.execute("SELECT * FROM model1.product_list")
+
+        for r in rs:
+            print(dict(r))
+            rpls= Plistserializer(data=dict(r))
+            if rpls.is_valid():
+                print('================================-===========================valid')
+        return Response(rpls.data,status=201)
     else:
         print('---------------------------------------not valid',sep='\n')
     return Response({})
