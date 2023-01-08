3f8ebfcbe8db0fcf59efc73bc4d873830c6f1206
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-08 12:34:44 +0530

    added display name in serializer

--- inventory/productapi/serializers.py
+++ inventory/productapi/serializers.py
@@ -4,15 +4,17 @@ import re
 class low(serializers.CharField):
     def to_internal_value(self, data):
         return re.sub(r'[^\w\s]', '', data.lower())
+
 color_choices=['','red','blue','black']
 required_iteam_choices=['row1','row2','row3']
 class Plistserializer(serializers.Serializer):
-    pname=low()
+    dname=serializers.CharField()
+    pname=low(required=False)
     color=serializers.ChoiceField(color_choices,default='black')
     required_iteams = serializers.ListField(child=serializers.ChoiceField(required_iteam_choices))
     category=low()
     pid=serializers.UUIDField()
-    # def validate(self, data):
-    #     if data['fname'] not in a:
-    #         raise serializers.ValidationError("fname is 0")
-    #     return data
+    
+    def validate(self, attrs):
+        attrs['pname'] = re.sub(r'[^\w\s]', '',attrs['dname'].lower())
+        return attrs
