8071f192110798e293c2c65890cca4115ed8a7aa
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-10 23:53:30 +0530

    for creating product

--- inventory/product_API/views.py
+++ inventory/product_API/views.py
@@ -2,17 +2,30 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .cqlqueries import ProductCQL, DatabaseError
 from django.shortcuts import redirect
+from .serializers import ProductListSerializer
 
 p = ProductCQL()
 
 
 class ProductList(APIView):
     def get(self, request):
-        # get all products
+        # get all product
         return Response(data=p.product_list(), status=200)
 
     def post(self, request):
         # code for creating product
+        sp = ProductListSerializer(data=request.data)
+        if sp.is_valid():
+            try:
+                pid = p.get_pid(pname=sp.data['pname'], color=sp.data['color'],
+                                required_iteams=sp.data['required_items'])
+                return Response(data={'error': 'Already Exist', 'pid': pid}, status=409)
+
+            except DatabaseError:
+
+                return Response(data={'error': 'Not Exist'}, status=409)
+        print(sp.data['pname'])
+
         return Response(data=p.product_list(), status=201)
 
 
