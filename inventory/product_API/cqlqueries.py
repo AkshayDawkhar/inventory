a917cfa50194336e58427bafb6d8f87ca30586a2
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-14 22:16:25 +0530

    cql querie to update the product by pid into product_list1_by_id

--- inventory/product_API/cqlqueries.py
+++ inventory/product_API/cqlqueries.py
@@ -58,10 +58,19 @@ class ProductCQL:
             r1 = self.session.execute(sp1, (pid, set(required_iteams), pname, color, dname, category,))
         return r.one()
 
+    def update_product(self, pid, pname, color, required_iteams, dname):
+
+        sp = self.session.prepare("UPDATE product_list1_by_id SET pname = ?, required_iteams =?, color = ?, "
+                                  "category = ?, dname = ? WHERE pid = ? IF EXISTS;")
+        a = self.session.execute(sp, (pname, color, required_iteams, dname, pid))
+        return a.one()
+
 
 if __name__ == "__main__":
     p = ProductCQL()
     # print(p.get_product(uuid.UUID('6555aec7-df75-4da4-b2a6-1c18907ff18b')))
     # print(p.prduct_list())
     # print(p.get_pid('media', 'red', ['row1', 'row2', 'row4', 'row3']))
-    print(p.create_product('media4', ['row1', 'row2', 'row3', 'row5'], 'black', 'category', 'dname'))
+    # print(p.create_product('media4', ['row1', 'row2', 'row3', 'row5'], 'black', 'category', 'dname'))
+    print(p.update_product(pid=uuid.UUID('410b3ccc-941d-11ed-af66-f889d2e641af'), pname='sounds', color='black',
+                           required_iteams=['row2', 'row2'], dname='sounds'))
