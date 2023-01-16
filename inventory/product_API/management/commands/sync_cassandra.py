2543d861256c3c14f3774540edbbc1b996dba37b
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-16 23:59:25 +0530

    having problem with creating  command for creating table and keyspace

--- inventory/product_API/management/commands/sync_cassandra.py
+++ inventory/product_API/management/commands/sync_cassandra.py
@@ -1,12 +1,6 @@
 import os
-import uuid
-
 from cassandra.cluster import Cluster
-from cassandra.cqlengine import connection
-from cassandra.cqlengine.management import sync_table, create_keyspace_simple
 from django.core.management.base import BaseCommand
-from product_API.models import ProductList
-
 
 def test_cassandra():
     # for a -->  UserWarning: CQLENG_ALLOW_SCHEMA_MANAGEMENT environment variable is not set. Future versions of this
@@ -15,14 +9,24 @@ def test_cassandra():
         os.environ['CQLENG_ALLOW_SCHEMA_MANAGEMENT'] = '1'
 
     cluster = Cluster(['127.0.0.1'])
-    sec = cluster.connect()
-    print('connected to the db')
-    con = connection.register_connection('cluster', session=sec)
-    create_keyspace_simple('model1', 1, connections=['cluster'])
-    print('synchronizing db ')
-    sync_table(ProductList)
-    mc = ProductList(pname='a', dname='A', required_items=['a', 'b'], category='mic', pid=uuid.uuid1())
-    mc.save()
+    session = cluster.connect()
+    print('Connected to the db')
+    # session.execute("CREATE TABLE IF NOT EXISTS  model1.product_list1 ( pname text ,color text , category text , dname text ,pid uuid , required_iteams frozen<set <text >> ,PRIMARY KEY (pname , required_iteams, color  )) WITH CLUSTERING ORDER BY (required_iteams ASC ) ;")
+    rows = session.execute(
+        "SELECT * FROM system_schema.keyspaces WHERE keyspace_name='model1'")
+
+    if rows:
+        session.execute(" DROP KEYSPACE IF EXISTS model1 ;")
+    session.execute("CREATE KEYSPACE model1 WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1 } ;")
+    session.execute("CREATE TABLE IF NOT EXISTS  model1.product_list1 ( pname text ,color text , category text , dname text ,pid uuid , required_iteams frozen<set <text >> ,PRIMARY KEY (pname , required_iteams, color  )) WITH CLUSTERING ORDER BY (required_iteams ASC ) ;")
+    session.execute("CREATE TABLE IF NOT EXISTS model1.product_list1_by_id (pid uuid PRIMARY KEY,category text,color text,dname text,pname text,required_items frozen<set<text>>)")
+    print('done')
+    # con = connection.register_connection('cluster', session=sec)
+    # create_keyspace_simple('model1', 1, connections=['cluster'])
+    # print('synchronizing db ')
+    # sync_table(ProductList)
+    # mc = ProductList(pname='a', dname='A', required_items=['a', 'b'], category='mic', pid=uuid.uuid1())
+    # mc.save()
 
 
 class Command(BaseCommand):
