395e3c864935b9480fd1b72c765ae3704822fcf6
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-16 21:41:59 +0530

    created serializer for updateing product

--- inventory/product_API/serializers.py
+++ inventory/product_API/serializers.py
@@ -65,7 +65,32 @@ class CreateProductSerializer(serializers.Serializer):
             pid = p.get_pid(pname=attrs['pname'], color=attrs['color'], required_items=attrs['required_items'])
             raise serializers.ValidationError({'error': 'Already Exist'})
         except DatabaseError:
-            p.create_product(pname=attrs['pname'], required_items =attrs['required_items'],
+            p.create_product(pname=attrs['pname'], required_items=attrs['required_items'],
                              color=attrs['color'], category=attrs['category'], dname=attrs['dname'])
             # raise serializers.ValidationError(self.error_massages, code='valid')
+        return attrs
+
+
+class UpdateProductSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
+    error_massages = {
+        'pname': 'ok',
+        'dname': 'ok',
+        'color': 'ok',
+        'required_items': [],
+        'category': 'ok',
+    }
+    pname = low(required=False)
+    dname = serializers.CharField()
+    color = serializers.ChoiceField(color_choices, default='black')
+    required_items = serializers.ListField(child=serializers.ChoiceField(required_item_choices))
+    category = serializers.CharField()
+
+    def validate(self, attrs):
+        attrs['pname'] = re.sub('[^A-Za-z0-9]+', '', attrs['dname'].lower())
         return attrs
