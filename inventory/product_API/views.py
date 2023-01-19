e233c0f3cb8b95ebcfa74e79d11482a7deef59ba
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-19 22:55:02 +0530

    change status codes

--- inventory/product_API/views.py
+++ inventory/product_API/views.py
@@ -2,14 +2,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .cqlqueries import ProductCQL, DatabaseError, NotFound, Conflict
 from .serializers import CreateProductSerializer, UpdateProductSerializer, InvalidDname
-
+from rest_framework import status
 p = ProductCQL()
 
 
 class ProductList(APIView):
     def get(self, request):
         # get all product
-        return Response(data=p.product_list(), status=200)
+        return Response(data=p.product_list(), status=status.HTTP_200_OK)
 
     def post(self, request):
         # code for creating product
@@ -18,7 +18,7 @@ class ProductList(APIView):
             return Response(data=p.product_list())
         # print(sp.errors)
 
-        return Response(sp.errors, status=226 if 'error' in sp.errors else 400)
+        return Response(sp.errors, status=status.HTTP_226_IM_USED if 'error' in sp.errors else status.HTTP_400_BAD_REQUEST)
 
 
 class Product(APIView):
@@ -27,7 +27,7 @@ class Product(APIView):
             a = p.get_product(pid=pid)
         except DatabaseError:
             return Response(data={'error': 'Product Not Found %s' % (pid,)},
-                            status=404)
+                            status=status.HTTP_404_NOT_FOUND)
         # print(a)
         return Response(a)
 
@@ -40,16 +40,16 @@ class Product(APIView):
                                      required_items=sp.data.get('required_items'), dname=sp.data.get('dname'),
                                      category=sp.data.get('category'))
             else:
-                return Response(data=sp.errors, status=400)
+                return Response(data=sp.errors, status=status.HTTP_400_BAD_REQUEST)
         except NotFound:
-            return Response(data={'error': 'Product Not Found %s' % (pid,)}, status=404)
+            return Response(data={'error': 'Product Not Found %s' % (pid,)}, status=status.HTTP_400_BAD_REQUEST)
         except InvalidDname:
-            return Response(data={"dname": ["Invalid Name"]}, status=406)
+            return Response(data={"dname": ["Invalid Name"]}, status=status.HTTP_406_NOT_ACCEPTABLE)
         except Conflict:
             return Response(
                 data={"error": "Same Product Exists %s\n%s\n%s" % (
                     sp.data.get('pname'), sp.data.get('color'), sp.data.get('required_items'))},
-                status=409)
+                status=status.HTTP_409_CONFLICT)
         return self.get(request, pid)
 
     def delete(self, request, pid):
@@ -57,32 +57,32 @@ class Product(APIView):
             p.delete_product(pid=pid)
         except NotFound:
             return Response(data={'error': 'Product Not Found %s' % (pid,)},
-                            status=404)
-        return Response(data=p.product_list(), status=202)
+                            status=status.HTTP_404_NOT_FOUND)
+        return Response(data=p.product_list(), status=status.HTTP_202_ACCEPTED)
 
 
 class Trashes(APIView):
     def get(self, request):
-        return Response(p.get_trashes(), status=200)
+        return Response(p.get_trashes(), status=status.HTTP_200_OK)
 
 
 class Trash(APIView):
     def get(self, request, pid):
         try:
             a = p.get_trash(pid)
-            return Response(a, status=200)
+            return Response(a, status=status.HTTP_200_OK)
         except NotFound:
-            return Response(data={'error': 'Not Found'}, status=404)
+            return Response(data={'error': 'Not Found'}, status=status.HTTP_404_NOT_FOUND)
 
     def post(self, request, pid):
         try:
             p.restore(pid)
-            return Response(data=p.get_trashes(), status=200)
+            return Response(data=p.get_trashes(), status=status.HTTP_200_OK)
         except Conflict:
-            return Response(data={'error': 'same product Already Exists '}, status=409)
+            return Response(data={'error': 'same product Already Exists '}, status=status.HTTP_409_CONFLICT)
         except DatabaseError:
-            return Response(data={'error': 'Product does not exists in Trash'}, status=404)
+            return Response(data={'error': 'Product does not exists in Trash'}, status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, pid):
         p.delete_trash(pid)
-        return Response(data=p.get_trashes(), status=200)
+        return Response(data=p.get_trashes(), status=status.HTTP_200_OK)
