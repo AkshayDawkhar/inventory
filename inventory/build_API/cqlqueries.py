de95bcd3d4b7431b7fc95e93009ed7a3b840d6a9
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-20 17:17:12 +0530

    added query's for geting build items and item

--- inventory/build_API/cqlqueries.py
+++ inventory/build_API/cqlqueries.py
@@ -2,20 +2,24 @@ from cassandra.cluster import Cluster
 from cassandra.cluster import dict_factory
 import uuid
 
+
 class BuildCQL:
     cluster = Cluster(['127.0.0.1'])
     session = cluster.connect('model1')
     session.row_factory = dict_factory
+    get_builds_query = session.prepare("SELECT * FROM product_builds ;")
+    get_build_query = session.prepare("SELECT * FROM product_builds WHERE pid = ? LIMIT 1;")
 
     def get_builds(self):
-        pass
-
-    def get_build(self, pid):
-        p = self.session.prepare("SELECT * FROM product_builds ;")
-        a = self.session.execute(p)
+        a = self.session.execute(self.get_builds_query)
         return a.all()
 
+    def get_build(self, pid):
+        a = self.session.execute(self.get_build_query, (pid,))
+        return a.one()
 
+
+# for testing the query's
 if __name__ == '__main__':
     b = BuildCQL()
     print(b.get_build(uuid.UUID('b11c39cf-0ced-45ed-88d4-015b9a3d4cfe')))
