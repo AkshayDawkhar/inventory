c13a829434ffe0b212702ea20914def1f1f0c6e1
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-20 17:31:40 +0530

    git combine cqlquery's with API endpoint

--- inventory/build_API/views.py
+++ inventory/build_API/views.py
@@ -1,13 +1,16 @@
 # Create your views here.
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from .cqlqueries import BuildCQL
+
+b = BuildCQL()
 
 
 class BuildProducts(APIView):
     def get(self, request):
-        return Response(data={}, status=200)
+        return Response(data=b.get_builds(), status=200)
 
 
 class BuildProduct(APIView):
     def get(self, request, pid):
-        return Response(data={}, status=200)
+        return Response(data=b.get_build(pid), status=200)
