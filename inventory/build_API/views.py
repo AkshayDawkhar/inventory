4d5350c6162f37201ec2796782ff5c3852116507
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-21 22:35:30 +0530

    change status codes

--- inventory/build_API/views.py
+++ inventory/build_API/views.py
@@ -5,13 +5,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .cqlqueries import BuildCQL, NotFound
 from .serializer import RequiredItemSerializer
-
+from rest_framework import status
 b = BuildCQL()
 
 
 class BuildProducts(APIView):
     def get(self, request):
-        return Response(data=b.get_builds(), status=200)
+        return Response(data=b.get_builds(), status=status.HTTP_200_OK)
 
 
 class BuildProduct(APIView):
@@ -19,18 +19,18 @@ class BuildProduct(APIView):
         try:
             r = b.get_build(pid)
         except NotFound:
-            return Response(data={'error': 'product Not Found'}, status=404)
-        return Response(data=r, status=200)
+            return Response(data={'error': 'product Not Found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(data=r, status=status.HTTP_200_OK)
 
 
 class RequiredItem(APIView):
     def get(self, request, pid):
         a = b.get_required_items(pid)
-        return Response(data=a, status=200)
+        return Response(data=a, status=status.HTTP_200_OK)
 
     def delete(self, request, pid):
         a = b.delete_required_items(pid)
-        return Response(data=a, status=200)
+        return Response(data=a, status=status.HTTP_200_OK)
 
 
 class RequiredItems(APIView):
@@ -39,6 +39,6 @@ class RequiredItems(APIView):
         if s.is_valid():
             b.create_required_items(pid=uuid.UUID(s.data.get('pid')), rid=uuid.UUID(s.data.get('rid')),
                                     numbers=s.data.get('numbers'))
-            return Response(data={}, status=200)
+            return Response(data={}, status=status.HTTP_200_OK)
         else:
-            return Response(data=s.errors, status=406)
+            return Response(data=s.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
