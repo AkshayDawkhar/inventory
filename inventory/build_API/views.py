ffa2a3ff873a1629a3e7991a570a3f36284fb71e
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-01-22 17:16:38 +0530

    created endpoint for deleting build_product

--- inventory/build_API/views.py
+++ inventory/build_API/views.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from .cqlqueries import BuildCQL, NotFound
 from .serializer import RequiredItemSerializer
 from rest_framework import status
+
 b = BuildCQL()
 
 
@@ -22,6 +23,12 @@ class BuildProduct(APIView):
             return Response(data={'error': 'product Not Found'}, status=status.HTTP_404_NOT_FOUND)
         return Response(data=r, status=status.HTTP_200_OK)
 
+    def delete(self, request, pid):
+        a = b.delete_build(pid)
+        if a:
+            return Response(data={}, status=201)
+        else:
+            return Response(data={'error': 'product Not Found'}, status=status.HTTP_404_NOT_FOUND)
 
 class RequiredItem(APIView):
     def get(self, request, pid):
