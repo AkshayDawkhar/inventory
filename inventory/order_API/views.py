117d01632ab84bce7b92e110acf2488b9540d3c1
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-02-03 23:05:40 +0530

    combine get_orders to API endpoint

--- inventory/order_API/views.py
+++ inventory/order_API/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from . import cqlqueries as order_cql
 
 # Create your views here.
 
 class Orders(APIView):
     def get(self, request):
-        return Response(data={}, status=status.HTTP_200_OK)
+        orders = order_cql.get_orders()
+        return Response(data=orders, status=status.HTTP_200_OK)
