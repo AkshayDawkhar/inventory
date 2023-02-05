dc34a91359c0a4c1cba62ea74f51cd831a199703
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-02-05 18:55:30 +0530

    added CQL for editing orders

--- inventory/order_API/cqlqueries.py
+++ inventory/order_API/cqlqueries.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 from cassandra.cluster import Cluster
 from cassandra.cluster import dict_factory
@@ -7,6 +8,8 @@ session = cluster.connect('model1')
 session.row_factory = dict_factory
 
 get_orders_query = session.prepare("SELECT pid,numbers,timestamp FROM orders ;")
+insert_into_orders_query = session.prepare(
+    "INSERT INTO orders (date , pid , timestamp , numbers ) VALUES ( ? , ?,?, ? ) ;")
 
 
 def get_orders():
@@ -14,6 +17,10 @@ def get_orders():
     return orders
 
 
+def edit_orders(pid, ts, numbers=1):
+    dt = datetime.fromtimestamp(ts)
+    session.execute(insert_into_orders_query, (dt, pid, dt, numbers))
+
+
 if __name__ == '__main__':
-    a = get_orders()
-    print(a)
+    a = edit_orders(uuid.UUID('db111c18-222b-4914-a108-b3e759239c5e'), 1675595420, 1)
