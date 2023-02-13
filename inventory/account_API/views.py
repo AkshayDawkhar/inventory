fd33627f2b9c97e5c8a7da8b23adb9fc340c9662
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-02-13 09:23:55 +0530

    created API endpoint for creating worker

--- inventory/account_API/views.py
+++ inventory/account_API/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from . import cqlqueries as accoutCQL
+from .serializers import CreateWorkerSerializer
 
 
 # Create your views here.
@@ -9,3 +10,9 @@ class accounts(APIView):
     def get(self, request):
         workers = accoutCQL.get_workers()
         return Response(data=workers, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = CreateWorkerSerializer(data=request.data)
+        if serializer.is_valid():
+            accoutCQL.create_worker(**serializer.data)
+        return Response(data=accoutCQL.get_workers(), status=status.HTTP_200_OK)
