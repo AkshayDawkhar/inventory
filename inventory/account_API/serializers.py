6163d12b0edc1bfd1e0dd3a81e39e1a1fdedadaf
Akshay <akshaydawkhar59@gmail.com>
2023-02-14 20:07:22 +0400

    changes in serilizers

--- inventory/account_API/serializers.py
+++ inventory/account_API/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 
 
 class CreateWorkerSerializer(serializers.Serializer):
+    username = serializers.CharField(default=None)
     f_name = serializers.CharField()
     l_name = serializers.CharField()
     password = serializers.CharField()
