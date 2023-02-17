d85cb873ab945b4306cde4d6d66ce69d847fb204
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-02-17 22:23:37 +0530

    change in format -> removed password from get API's of accounts

--- inventory/account_API/cqlqueries.py
+++ inventory/account_API/cqlqueries.py
@@ -1,29 +1,31 @@
-import uuid
 from cassandra.cluster import Cluster
 from cassandra.cluster import dict_factory
 
 cluster = Cluster(['127.0.0.1'])
 session = cluster.connect('model1')
 session.row_factory = dict_factory
-get_workers_query = session.prepare("SELECT * FROM user_worker ;")
-get_admins_query = session.prepare("SELECT * FROM user_admin ;")
+get_workers_query = session.prepare("SELECT username , fname , lname FROM user_worker ;")
+get_admins_query = session.prepare("SELECT username , fname , lname FROM user_admin ;")
 create_worker_query = session.prepare(
     "INSERT INTO user_worker (username , fname , lname , password  ) VALUES ( ?, ?,?, ?) IF NOT EXISTS;")
 create_admin_query = session.prepare(
     "INSERT INTO user_admin (username , fname , lname , password  ) VALUES ( ?, ?,?, ?) IF NOT EXISTS;")
-get_worker_query = session.prepare("SELECT * FROM user_worker WHERE username = ? LIMIT 1; ")
-get_admin_query = session.prepare("SELECT * FROM user_admin WHERE username = ? LIMIT 1; ")
+get_worker_query = session.prepare("SELECT username , fname , lname FROM user_worker WHERE username = ? LIMIT 1; ")
+get_admin_query = session.prepare("SELECT username , fname , lname FROM user_admin WHERE username = ? LIMIT 1; ")
+
 
 class AlreadyExists(Exception):
     pass
 
-def get_workers(username = None):
+
+def get_workers(username=None):
     if username is None:
         workers = session.execute(get_workers_query)
         return workers.all()
     else:
-        worker = session.execute(get_worker_query,(username,)).one()
+        worker = session.execute(get_worker_query, (username,)).one()
         return worker
+
 
 def create_worker(f_name, l_name, password, username=None):
     if username is None:
@@ -31,19 +33,22 @@ def create_worker(f_name, l_name, password, username=None):
     a = session.execute(create_worker_query, (username, f_name, l_name, password)).one()
     return a['[applied]']
 
-def get_admins(username = None):
+
+def get_admins(username=None):
     if username is None:
         admin = session.execute(get_admins_query)
         return admin.all()
     else:
-        admin = session.execute(get_admin_query,(username,)).one()
+        admin = session.execute(get_admin_query, (username,)).one()
         return admin
+
 
 def create_admin(f_name, l_name, password, username=None):
     if username is None:
         username = f_name + l_name
     a = session.execute(create_admin_query, (username, f_name, l_name, password)).one()
     return a['[applied]']
+
 
 if __name__ == '__main__':
     print(get_workers())
