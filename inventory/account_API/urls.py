a33b15f9eb75b8784c9be32d4c896bafe40a2985
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-02-17 11:17:38 +0530

    fixed urls for account

--- inventory/account_API/urls.py
+++ inventory/account_API/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.accounts.as_view()),
-    path('/<String:username>',views.account.as_view()),
-    path('admin/',views.admins.as_view()),
-    path('admin/<String:username>',views.admin.as_view()),
-    ]
+    path('<str:username>', views.account.as_view()),
+    path('admin/', views.admins.as_view()),
+    path('admin/<str:username>', views.admin.as_view()),
+]
