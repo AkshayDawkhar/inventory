8d763bb0bc91557d525baf378cadb92e4b912d0b
akshaydawkhar <akshaydawkhar59@gmail.com>
2023-02-23 11:49:37 +0530

    create funtions for generate unique random username

--- inventory/account_API/utility.py
+++ inventory/account_API/utility.py
@@ -1,11 +1,22 @@
 import random
-import string
+import cqlqueries as accountCQL
 
 
-def generate_username(f_name, l_name):
-    username = f_name + l_name + str(random.randint(a=0, b=100))
+def generate_username(f_name, l_name, b):
+    username = f_name + l_name + str(random.randint(a=0, b=b))
+    return username
+
+
+def generate_unique_usernames(f_name, l_name):
+    username = f_name + l_name
+    b = 0
+    while accountCQL.get_worker_username(username):
+        b = b+10
+        username = generate_username(f_name, l_name, b=b)
+        print(username, b)
     return username
 
 
 if __name__ == '__main__':
-    print(generate_username('a', 'b'))
+    # print(generate_username('a', 'b', 12))
+    print(generate_unique_usernames('mongo', 'db'))
